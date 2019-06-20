91311592722f557d0a7dd615802f2ed5b143b8fc
Gideon Bar <gbar@marketo.com>
2019-06-20 22:30:46 +0300

    slate image packing enables polymorphism i.e. with or without mount

--- src/wield_services/deploy/slate/wield/slate_image.py
+++ src/wield_services/deploy/slate/wield/slate_image.py
@@ -1,29 +1,38 @@
 #!/usr/bin/env python
 
-from wield_services.wield.deploy.util import pack_image, get_conf_context_project, get_module_root
+from wielder.util.imager import pack_image
+from wield_services.wield.deploy.util import get_conf_context_project, \
+    get_module_root, get_project_image_root, get_project_root
 
 
 def slate_image():
 
-    conf = get_conf_context_project()
+    project_root = get_project_root()
+    conf = get_conf_context_project(project_root=project_root)
 
     pack_image(
-        conf,
+        conf=conf,
         name='py37',
+        image_root=get_project_image_root(),
         push=False,
-        force=False
+        force=False,
+        tag='dev'
     )
 
     module_root = get_module_root(__file__)
 
     image_root = f'{module_root}image'
 
+    code_path = f'{image_root}/code_path'
+
     pack_image(
         conf,
         name='slate',
+        image_root=image_root,
         push=False,
         force=True,
-        image_root=image_root
+        tag='dev',
+        mount=True
     )
 
     # push_image(conf)
