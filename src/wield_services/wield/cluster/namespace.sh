3398787505ef68c97ae379c8230ef2f83459a058
Gideon Bar <gbar@marketo.com>
2019-07-01 09:07:01 +0300

    namespace configurable and overridable facilitating local dev

---
+++ src/wield_services/wield/cluster/namespace.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# TODO make this work or delete it
+kubectl config set-context wielder-services --namespace=wielder-services \
+  --cluster=docker-for-desktop-cluster \
+  --user=docker-for-desktop
