d402f526f628084ffc97aa7db731f48ae310b07b
Gideon <hamshif@gmail.com>
2019-10-13 17:17:50 +0300

    create multi topic producer

--- src/wield_services/deploy/slate/image/code_path/producer1.py
+++ src/wield_services/deploy/slate/image/code_path/producer1.py
@@ -2,7 +2,7 @@
 
 from kafka import KafkaConsumer, KafkaProducer
 
-KAFKA_TOPIC = 'demo'
+KAFKA_TOPICS = ['telegraf', 'demo']
 # KAFKA_BROKERS = 'localhost:32400' # see step 1
 
 # from inside the cluster in a different namespace
@@ -12,7 +12,7 @@ KAFKA_BROKERS = 'wielder-kafka.kafka.svc.cluster.local:9092'
 
 # KAFKA_BROKERS = 'bootstrap:9092'
 
-print(f'KAFKA_BROKERS: {KAFKA_BROKERS}\n Topic {KAFKA_TOPIC}')
+[print(f'KAFKA_BROKERS: {KAFKA_BROKERS}\n Topic {t}') for t in KAFKA_TOPICS]
 
 producer = KafkaProducer(bootstrap_servers=KAFKA_BROKERS)
 
@@ -29,8 +29,12 @@ messages = [
 # pt = producer.partitions_for(KAFKA_TOPIC)
 # print(f'partitions for {KAFKA_TOPIC}: {pt}')
 for m in messages:
-    print(f"sending: {m} to topic: {KAFKA_TOPIC}")
-    producer.send(topic=KAFKA_TOPIC, value=m)
+
+    for t in KAFKA_TOPICS:
+
+        print(f"sending: {m} to topic: {t}")
+        producer.send(topic=t, value=m)
+        producer.send(topic=t, value=m)
 
 producer.flush()
 
